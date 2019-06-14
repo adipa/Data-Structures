d2a05626e47fa0f5e52ef764c947235c5f926446
adipa <aditipatil110899@gmail.com>
2019-06-14 14:43:38 +0530

    anagram updated

--- src/anagram.c
+++ src/anagram.c
@@ -1,43 +1,36 @@
 //check if two strings are anagrams of each other
 #include<stdio.h>
-#include<string.h>
 
 #define MAX_SIZE 20
 #define ASCII_LENGTH 256
 
-int anagram(char *str1, char *str2);
-void count(char *str, int ascii_array[]);
+int areAnagrams(char *str1, char *str2);
 
 int main()
 {
     char str1[MAX_SIZE], str2[MAX_SIZE];
     scanf("%s%s", str1, str2);
-    if(anagram(str1, str2))
+    if(areAnagrams(str1, str2))
         printf("Anagram");
     else
         printf("Not an anagram");
 }
 
-int anagram(char *str1, char *str2)
+int areAnagrams(char *str1, char *str2)
 {
-    int i, ascii_array1[ASCII_LENGTH] = {0}, ascii_array2[ASCII_LENGTH] = {0};
-    //size has to be same for strings to be anagrams
-    if(strlen(str1) != strlen(str2))
-        return 0;
-    //count occurrences of each character in both the strings
-    count(str1, ascii_array1);
-    count(str2, ascii_array2);
-    //strings are anagrams of each other only if count of all the character match
-    for(i = 0; i < ASCII_LENGTH; i++){
-        if(ascii_array1[i] != ascii_array2[i])
+    int i, ascii_array[ASCII_LENGTH] = {0};
+    for(i = 0; str1[i] != '\0' || str2[i] != '\0'; i++) {
+        //increment the value at the character index by 1
+        ascii_array[str1[i]] += 1;
+        //decrement the value at the character index by 1
+        ascii_array[str2[i]] -= 1;
+    }
+    //0 value at index indicates equal character count in both the strings
+    for(i = 0;i < ASCII_LENGTH; i++) {
+        if(ascii_array[i] != 0)
             return 0;
     }
     return 1;
 }
 
-void count(char *str, int ascii_array[])
-{
-    int i = 0;
-    while(str[i] != '\0')
-        ascii_array[str[i++]] += 1;
-}
+
