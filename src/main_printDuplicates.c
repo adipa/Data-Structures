280339a1239f203a5363904f1d61641f60b6eeab
Aditi Patil <apatil@localhost.localdomain>
2019-07-01 07:55:54 +0530

    Updated main.c

--- src/main_printDuplicates.c
+++ src/main_printDuplicates.c
@@ -1,16 +1,16 @@
 /* Print duplicate characters from string */
-#include "printDuplicates.h"
+#include "printDuplicates2.h"
 
 #define ASCII_CHARACTER_LENGTH 256 
 
-char *printDuplicates(char *str, char newStr[])
+int printDuplicates(char *str, int duplicates[])
 {
     int ascii[ASCII_CHARACTER_LENGTH] = {0}, i, j;
     for (i = 0, j = 0; str[i] != '\0'; i++) {
       ascii[(int) str[i]] += 1;
       if (ascii[(int) str[i]] == 2 && str[i] != ' ')
-            newStr[j++] = str[i];
+            duplicates[j++] = str[i];
     }
 
-    return newStr;
+    return j - 1;
 }
---
+++ src/usage.c
@@ -0,0 +1,10 @@
+x#include "usage.h"
+
+void usage(char *filename)
+{
+  printf("Usage %s <Options> <String1> <String2(optional)> \
+  \nOptions: \
+  \n\t maxchar \n\t\t-Maximum Occuring Character \n\t prdup \n\t\t-Print Duplicates \
+  \n\t rmdup \n\t\t-Remove Duplicates from string \n\t pal \n\t\t-Check for Palindrome \
+  \n\t anagrams \n\t\t-Check for Anagram", filename);
+}
