367b55f52c60bce6903afa9346fba25faba5896e
Aditi Patil <apatil@localhost.localdomain>
2019-07-09 03:33:48 +0530

    Data Structures

---
+++ src/strings/8.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+char first(char* str);
+
+int main()
+{
+    char str[20],c;
+    printf("Enter String: ");
+    scanf("%[^\n]s",str);
+    c=first(str);
+    if(c=='\0')
+        printf("No non-repeating character.");
+    else
+        printf("First non-repeating character: %c",c);
+    return 0;
+}
+
+char first(char* str)
+{
+    int i=0,j=0;
+    while(str[i]!='\0'){
+        if(str[i]==str[j] && i!=j)
+        {
+            i++;j=0;
+        }
+        else j++;
+        if(str[j]=='\0' && str[i]!=' ')
+            break;
+    }
+    return str[i];
+}
---
+++ src/strings/main_areRotations.c
@@ -0,0 +1,33 @@
+//Check if two strings are rotations of each other
+
+#include "areRotations.h"
+
+#define true 1
+#define false 0
+
+int areRotations(char* str1,char* str2)
+{
+    int i = 0, j = 0;
+    if(str1 == str2)         
+        return true;
+    if(strlen(str1) != strlen(str2))
+        return false;
+
+    //Find first character of str1 in str2.
+    while((str2[i] != str1[0]) && (str2[i] != '\0'))  
+            i++;
+
+    //Find first character of str1 in str2.
+    if(str2[i] == '\0')                   
+        return false;
+
+    while(str1[j] != '\0') {
+        //Till str1 is not traversed, check if every char in str1 matches str.
+        if(str1[j++] != str2[i++])
+            return false;
+	//Come to first position when str2 ends.
+        if(str2[i] == '\0') 
+            i = 0;
+    }        
+    return true;          
+}
