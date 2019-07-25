ebf6588c7b1e74ae0b4e27a24017b658798bf2b3
Aditi Patil <aditipatil110899@gmail.com>
2019-07-25 17:23:17 +0530

    Update linklist: main.c

--- src/linkedLists/main.c
+++ src/linkedLists/main.c
@@ -1,18 +1,22 @@
 #include "linkList.h"
 
+#define MAX 15
+#define INDEX 5
+#define MIDDLE 100
+
 int main()
 {
   int i;
   Node *head = NULL;
-  for (i = 0; i < 15; i++)
+  for (i = 0; i < MAX; i++)
     head = createList(head, createNode(i));
   printf("Linklist:\n");
   printList(head);
   printf("\nAccessing Linklist using indexing\n");
-  printf("\nNode at index %d: %d\n", 10, linkListIndexing(head, 10));
+  printf("\nNode at index %d: %d\n", 10, linkListIndexing(head, INDEX));
   printf("\nLength of Linklist: %d\n",length(head,0));
   printf("\nInsert Node in middle of LL\n");
-  head = insertNodeInMiddle(head, createNode(100));
+  head = insertNodeInMiddle(head, createNode(MIDDLE));
   printList(head);
   printf("\nReversed Linklist:\n");
   printList(reverseList(head));
